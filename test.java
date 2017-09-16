390754ef431a54abcd2f442de2425ebed0c58aea
Neil Opena <neil1114o@gmail.com>
2017-09-16 11:58:32 -0400

    Created a test java file to edit with vim

---
+++ test.java
@@ -0,0 +1,16 @@
+public class test{
+	public static void main(String[] args){
+		System.out.println("Hello, this is just a test to see if vim actually compliles");
+
+
+		for(int i = 0; i < 4; i++){
+			System.out.println("Gitbash is so cool");	
+		
+		} 
+
+		System.out.println((int)Math.random()* 20);
+	
+	}
+
+}
+
