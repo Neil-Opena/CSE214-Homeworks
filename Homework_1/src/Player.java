7ce4a39fde6b0690a967a8a5d1d7c34308eb5511
Neil Opena <neil1114o@gmail.com>
2017-09-16 16:21:49 -0400

    Finished player sort method

--- Homework_1/src/Player.java
+++ Homework_1/src/Player.java
@@ -1,2 +1,38 @@
 public class Player {
+
+    private Card[] cards;
+    private int id;
+
+    public Player(int id){
+        this.id = id;
+    }
+
+    public void setCards(Card[] cards){
+        this.cards = cards;
+    }
+
+    public void printCards(){
+        for(int i = 0; i < cards.length; i++){
+            System.out.print(cards[i] + " ");
+        }
+    }
+
+    public void sortCards(){
+        //selection sort
+        for(int i = 0; i < cards.length; i++){
+            int max = cards[i].getCArdValue();
+            int maxIndex =  i;
+            for(int j = i + 1; j < cards.length; j++){
+                if(cards[j].getCArdValue() > max){
+                   max = cards[j].getCArdValue();
+                   maxIndex = j;
+                }
+            }
+            Card temp = cards[i];
+            cards[i] = cards[maxIndex];
+            cards[maxIndex] = temp;
+        }
+    }
 }
+
+
--- Homework_1/src/Test.java
+++ Homework_1/src/Test.java
@@ -14,7 +14,39 @@ public class Test {
         System.out.println("Triplets");
         System.out.println(ComplexityAnalysis.tripletsProgram(arr, key));
 
+        System.out.println("========================================");
+
+        //CreditCard
+        CreditCard a = new CreditCard("1234","Neil","Bank",300,500.00);
+        System.out.println(a);
+
+        System.out.println("========================================");
+
+        //Cards
+        cardSortingTest();
+    }
+
+    public static void cardSortingTest(){
+        String[] cardsForPlayer1 = {
+                                        "S4", "D8", "C4", "D3", "D5", "DJ", "S3", "D4", "DA", "SJ",
+                                        "D7", "H10", "D6"
+                                    };
+        Card[] cards = new Card[13];
+        for(int i = 0; i < cardsForPlayer1.length; i++){
+           Card mCard = new Card(cardsForPlayer1[i]);
+           cards[i] = mCard;
+        }
+
+        int id = 1;
+        Player player = new Player(id);
+        player.setCards(cards);
+        player.printCards();
+
+        System.out.println();
+
+        player.sortCards();
 
 
+        player.printCards();
     }
 }
