b00f9d21f1359bf8ef12154815c56301fe8cb013
Neil Opena <neil1114o@gmail.com>
2017-10-09 17:44:41 -0400

    Finished testing for TeamSelection program

--- Homework_2/src/TeamSelection.java
+++ Homework_2/src/TeamSelection.java
@@ -1,25 +1,33 @@
 import java.io.*;
 import java.util.*;
 public class TeamSelection {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //read input from fileA
-        try{
+        try {
             File file = new File("in2.txt");
             Scanner input = new Scanner(file);
 
             int numTests = Integer.parseInt(input.nextLine());
 
-            for(int i = 0; i < numTests; i++){
-                PlayerLinkedList temp = new PlayerLinkedList(input.nextLine(),input.nextLine());
-                temp.printJerseyNums();
-                temp.printHeights();
+            for (int i = 0; i < numTests; i++) {
+                PlayerLinkedList temp = new PlayerLinkedList(input.nextLine(), input.nextLine());
+                /*temp.printJerseyNums();
+                temp.printHeights();*/
                 temp.eliminate();
                 temp.printJerseyNums();
+                System.out.println("----------------------------");
             }
 
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("File does not exist. Please place a file named \"in2.txt\" in the " +
                     "same directory as the src directory, and try again.");
+        }catch(NullPointerException e){
+            e.printStackTrace();
+            System.out.println("Some error occurred...");
+        }catch(Exception e){
+            System.out.println("File is not formatted correctly. Please format it such that the first line " +
+            "contains number of test cases. Second line contains the jersey number of the players. Third line " +
+            "contains heights of the players in centimeters.");
         }
 
     }
@@ -119,11 +127,10 @@ class PlayerLinkedList {
         if (head == null) {
             System.out.println("List is empty. There are no players");
         }
-        System.out.println("Will begin eliminating...");
         Node current = head;
         while(current.next!=null){
            if(current.next.player.getHeight() > current.player.getHeight()){
-               System.out.println(current + " will be eliminated");
+               //System.out.println(current + " will be eliminated");
                removeNode(current);
            }
            current = current.next;
@@ -131,7 +138,10 @@ class PlayerLinkedList {
     }
 
     private void removeNode(Node node){
+        if(head == node){
+            head = head.next;
+        }else {
             node.previous.next = node.next;
-
+        }
     }
 }
