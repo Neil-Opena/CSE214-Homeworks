66cf0215a5a81b4ac8b26851b59653ad94938a63
Neil Opena <neil1114o@gmail.com>
2017-11-20 16:11:49 -0500

    Main functionaility fixed

--- Homework_4/src/Question_2.java
+++ Homework_4/src/Question_2.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 public class Question_2 {
 
@@ -12,158 +13,106 @@ public class Question_2 {
         int M = Integer.parseInt(temp[1]);
 
         int[] data = new int[N];
-        int[] original = new int[N];
         for(int i = 0; i < N; i++) {
             data[i] = Integer.parseInt(dataString[i]);
-            original[i] = Integer.parseInt(dataString[i]);
         }
 
-        ArrayList<Value> listSums = new ArrayList<>();
+        HashMap map = new HashMap(N * N);
         for(int i = 0; i < N; i++){
-            for(int j = i + 1; j < N; j++){ // O(n^2)
-                Value tempValue = new Value(original[i] + original[j], i, j); // create ArrayList of possible sums
-                listSums.add(tempValue);
-            }
-        }
+            for(int j = i + 1; j < N; j++){
+                //instead of one value stored, perhaps an array
 
-        HashMap map;
-        if(N > 16){
-            map = new HashMap(N + (int) (0.75 * N));
-            // create a new HashMap with a larger capacity
-            // 75% of N to decrease the load factor
-        }else{
-            map = new HashMap();
-        }
+                Value tempValue = new Value(data[i] + data[j], i, j);
+                int key = tempValue.getSum() % M;
 
-        for(int i = 0; i < N; i++){
-            map.put(original[i],i); // put original array into hashMap
-        }
-
-        int maxTrio = findMaxTrio(data);
-        int[] multiplesOfM = getMultiplesOfM(maxTrio, M);
-        HashMap answerMap = new HashMap(listSums.size());
-
-        for(int i = 0; i < listSums.size(); i++){
-            Value testValue = listSums.remove(i);
-            for(int j = 0; j < multiplesOfM.length; j++){ // O(n^2)
-                int key = multiplesOfM[j] - testValue.getSum();
-                if(key < 0) continue;
-                Integer retrievedValue = (Integer) map.get(key);
-                if(retrievedValue != null && (retrievedValue != testValue.getIndex1()) && (retrievedValue != testValue.getIndex2())){
-
-                    Answer tempAnswer = new Answer(testValue.getIndex1(), testValue.getIndex2(), retrievedValue);
-                    tempAnswer.sortTrio();
-                    String test = tempAnswer.getStringRepresentation();
-
-                    //FIXME remove?
-
-                    answerMap.putIfAbsent(test, tempAnswer);
+                ArrayList<Value> testList = (ArrayList<Value>) map.get(key);
+                if(testList != null){
+                    // get arrayList
+                    testList.add(tempValue);
+                    map.put(key, testList);
+                }else{
+                    ArrayList<Value> valueList = new ArrayList<>();
+                    valueList.add(tempValue);
+                    map.put(key, valueList);
                 }
-                //FIXMe other sums
             }
         }
 
-        Object[] answers = answerMap.values().toArray();
-        System.out.println(answers.length);
+        int key;
+        ArrayList<Answer> answerList = new ArrayList<>();
 
-        //For testing purposes only
-        Arrays.sort(answers);
-        for(int i = 0; i < answers.length; i++){
-            System.out.println(answers[i]);
-        }
+        for(int i = 0; i < N; i++) {
+            //look for value st.
+            key = (data[i] % M);
+            if (key != 0) {
+                key = M - key;
+            }
 
-        //FIXMe add fix to array size check
+            ArrayList<Value> retrievedList = (ArrayList<Value>) map.get(key);
 
-
-    }
-
-    //if hashmap.get answer is null --> put value in hashmap
-
-    public static int findMaxTrio(int[] arr){
-        // sorts array to find max Trio
-        for(int i = 1; i < arr.length; i++){
-            int index = i;
-            while(index > 0 && arr[index - 1] > arr[index]){ //sort input
-                int tempSort = arr[index - 1];
-                arr[index - 1] = arr[index];
-                arr[index] = tempSort;
-                index--;
+            if (retrievedList != null) {
+                for (int j = 0; j < retrievedList.size(); j++) {
+                    Value temporaryValue = retrievedList.get(j);
+                    if (temporaryValue.getIndex2() != i && temporaryValue.getIndex1() != i) {
+                        Answer answer = new Answer(temporaryValue, i);
+                        answer.sortTrio();
+                        System.out.println(answer);
+                    }
+                }
             }
         }
-        return arr[arr.length - 1] + arr[arr.length - 2] + arr[arr.length - 3];
-    }
-
-    public static int[] getMultiplesOfM(int maxValue, int M){
-        int[] temp = new int[maxValue / M];
-        for(int i = 0; i < temp.length; i++){
-            temp[i] = M * (i + 1);
-        }
-
-        return temp;
     }
 
 }
 
-class Answer implements Comparable{
-    private int index1;
-    private int index2;
-    private int index3;
 
-    public Answer(int index1, int index2, int index3){
-        this.index1 = index1;
-        this.index2 = index2;
-        this.index3 = index3;
+class Answer {
+    private Value value;
+    private int index;
+
+    public Answer(Value value, int index){
+        this.value = value;
+        this.index = index;
     }
 
-    public int getIndex3() {
-        return index3;
+    public Value getValue(){
+        return this.value;
     }
 
-    public int getIndex2() {
-        return index2;
-    }
-
-    public int getIndex1() {
-        return index1;
-    }
-
-    public String getStringRepresentation() {
-        return "" + index1 + index2 + index3;
+    public String getString(){
+        return "" + value.getIndex1() + value.getIndex2() + index;
     }
 
     public String toString(){
-        return ("[" + index1 + ", " + index2 + ", " + index3 + "]");
+        return ("[" + value.getIndex1() + ", " + value.getIndex2() + ", " + index + "]");
     }
 
     public void sortTrio(){
-        if(index1 <= index2 && index1 <= index3){
+        if(value.getIndex1() <= value.getIndex2() && value.getIndex1() <= index){
             //min = index1
             //no swap necessary for index1
-        }else if(index2 <= index1 && index2 <= index3){
+        }else if(value.getIndex2() <= value.getIndex1() && value.getIndex2() <= index){
             //min = index2
-            int temp = index1;
-            index1 = index2;
-            index2 = temp;
+            int temp = value.getIndex1();
+            value.setIndex1(value.getIndex2());
+            value.setIndex2(temp);
 
         }else{
-            //min = index3
-            int temp = index1;
-            index1 = index3;
-            index3 = temp;
+            //min = index
+            int temp = value.getIndex1();
+            value.setIndex1(index);
+            index = temp;
         }
 
-        if(index2 <= index3){
+        if(value.getIndex2() <= index){
             //no swap necessary
         }else{
-            int temp = index2;
-            index2 = index3;
-            index3 = temp;
+            int temp = value.getIndex2();
+            value.setIndex2(index);
+            index = temp;
         }
     }
 
-    public int compareTo(Object answer){
-        return Integer.compare(Integer.parseInt(getStringRepresentation()), Integer.parseInt(((Answer) answer).getStringRepresentation()));
-    }
 }
 
 class Value{
@@ -177,6 +126,10 @@ class Value{
         this.index2 = index2;
     }
 
+    public String toString(){
+        return "sum=" + sum + ", index1=" + index1 +", index2=" + index2;
+    }
+
     public int getSum(){
         return sum;
     }
@@ -187,6 +140,14 @@ class Value{
 
     public int getIndex2(){
         return index2;
+    }
+
+    public void setIndex1(int index1){
+        this.index1 = index1;
+    }
+
+    public void setIndex2(int index2){
+        this.index2 = index2;
     }
 }
 
