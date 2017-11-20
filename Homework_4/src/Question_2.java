7fdcb0d2b163ec885ff74ed0b2f075d543f189aa
Neil Opena <neil1114o@gmail.com>
2017-11-20 02:24:44 -0500

    Fixed duplicate issue

--- Homework_4/src/Question_2.java
+++ Homework_4/src/Question_2.java
@@ -12,26 +12,21 @@ public class Question_2 {
         int M = Integer.parseInt(temp[1]);
 
         int[] data = new int[N];
-        for(int i = 0; i < N; i++){
+        int[] original = new int[N];
+        for(int i = 0; i < N; i++) {
             data[i] = Integer.parseInt(dataString[i]);
+            original[i] = Integer.parseInt(dataString[i]);
         }
 
-        /*
-
-        int[][] matrix = new int[N][N]; // create a matrix of possible sums of numbers
-
+        ArrayList<Value> listSums = new ArrayList<>();
         for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){ // O(n^2)
-                if(i == j){
-                    matrix[i][j] = data[i];
-                }else{
-                    matrix[i][j] = data[i] + data[j];
-                }
+            for(int j = i + 1; j < N; j++){ // O(n^2)
+                Value tempValue = new Value(original[i] + original[j], i, j); // create ArrayList of possible sums
+                listSums.add(tempValue);
             }
-        } //FIXMe might not need to initialize matrix
+        }
 
         HashMap map;
-
         if(N > 16){
             map = new HashMap(N + (int) (0.75 * N));
             // create a new HashMap with a larger capacity
@@ -41,58 +36,75 @@ public class Question_2 {
         }
 
         for(int i = 0; i < N; i++){
-            map.put(data[i],i);
+            map.put(original[i],i); // put original array into hashMap
         }
 
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(i == j){
-                    continue;
+        int maxTrio = findMaxTrio(data);
+        int[] multiplesOfM = getMultiplesOfM(maxTrio, M);
+        HashMap answerMap = new HashMap(listSums.size());
+
+        for(int i = 0; i < listSums.size(); i++){
+            Value testValue = listSums.remove(i);
+            for(int j = 0; j < multiplesOfM.length; j++){ // O(n^2)
+                int key = multiplesOfM[j] - testValue.getSum();
+                if(key < 0) continue;
+                Integer retrievedValue = (Integer) map.get(key);
+                if(retrievedValue != null && (retrievedValue != testValue.getIndex1()) && (retrievedValue != testValue.getIndex2())){
+
+                    Answer tempAnswer = new Answer(testValue.getIndex1(), testValue.getIndex2(), retrievedValue);
+                    tempAnswer.sortTrio();
+                    String test = tempAnswer.getStringRepresentation();
+
+                    //FIXME remove?
+
+                    answerMap.putIfAbsent(test, tempAnswer);
                 }
-                int tempKey;
-
-                tempKey = (matrix[i][j] + M);
-                int productOfM = tempKey - (tempKey % M);
-                int key = productOfM - matrix[i][j];
-
-
-                Integer tempAnswer = (Integer) map.get(key);
-                if(tempAnswer != null && tempAnswer != i){
-                    Answer a = new Answer(i,j,tempAnswer);
-                    System.out.println(a);
-                }
-
+                //FIXMe other sums
             }
-        } */
+        }
 
-        //FIXMe print num of trios
+        Object[] answers = answerMap.values().toArray();
+        System.out.println(answers.length);
 
-        for(int i = 1; i < N; i++){
-            int index = i;
-            while(index > 0 && data[index - 1] > data[index]){ //sort input
-                int tempSort = data[index - 1];
-                data[index - 1] = data[index];
-                data[index] = tempSort;
-                index--;
-            }
+        //For testing purposes only
+        Arrays.sort(answers);
+        for(int i = 0; i < answers.length; i++){
+            System.out.println(answers[i]);
         }
 
         //FIXMe add fix to array size check
-        Answer maxTrio = new Answer(data[N - 1], data[N - 2], data[N -3]);
-
-        int[] multiplesOfM = new int[maxTrio.sum()/M];
-        for(int i = 0; i < multiplesOfM.length; i++){
-            multiplesOfM[i] = M * (i + 1);
-            System.out.print(multiplesOfM[i] + " ");
-        }
-
 
 
     }
+
+    //if hashmap.get answer is null --> put value in hashmap
+
+    public static int findMaxTrio(int[] arr){
+        // sorts array to find max Trio
+        for(int i = 1; i < arr.length; i++){
+            int index = i;
+            while(index > 0 && arr[index - 1] > arr[index]){ //sort input
+                int tempSort = arr[index - 1];
+                arr[index - 1] = arr[index];
+                arr[index] = tempSort;
+                index--;
+            }
+        }
+        return arr[arr.length - 1] + arr[arr.length - 2] + arr[arr.length - 3];
+    }
+
+    public static int[] getMultiplesOfM(int maxValue, int M){
+        int[] temp = new int[maxValue / M];
+        for(int i = 0; i < temp.length; i++){
+            temp[i] = M * (i + 1);
+        }
+
+        return temp;
+    }
+
 }
 
-
-class Answer{
+class Answer implements Comparable{
     private int index1;
     private int index2;
     private int index3;
@@ -103,12 +115,78 @@ class Answer{
         this.index3 = index3;
     }
 
-    public int sum(){
-        return index1 + index2 + index3;
+    public int getIndex3() {
+        return index3;
+    }
+
+    public int getIndex2() {
+        return index2;
+    }
+
+    public int getIndex1() {
+        return index1;
+    }
+
+    public String getStringRepresentation() {
+        return "" + index1 + index2 + index3;
     }
 
     public String toString(){
-        return "[" + index1 + ", " + index2 + ", " + index3 + "]";
+        return ("[" + index1 + ", " + index2 + ", " + index3 + "]");
     }
 
+    public void sortTrio(){
+        if(index1 <= index2 && index1 <= index3){
+            //min = index1
+            //no swap necessary for index1
+        }else if(index2 <= index1 && index2 <= index3){
+            //min = index2
+            int temp = index1;
+            index1 = index2;
+            index2 = temp;
+
+        }else{
+            //min = index3
+            int temp = index1;
+            index1 = index3;
+            index3 = temp;
+        }
+
+        if(index2 <= index3){
+            //no swap necessary
+        }else{
+            int temp = index2;
+            index2 = index3;
+            index3 = temp;
+        }
+    }
+
+    public int compareTo(Object answer){
+        return Integer.compare(Integer.parseInt(getStringRepresentation()), Integer.parseInt(((Answer) answer).getStringRepresentation()));
+    }
 }
+
+class Value{
+    private int sum;
+    private int index1;
+    private int index2;
+
+    public Value(int sum, int index1, int index2){
+        this.sum = sum;
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    public int getSum(){
+        return sum;
+    }
+
+    public int getIndex1(){
+        return index1;
+    }
+
+    public int getIndex2(){
+        return index2;
+    }
+}
+
