eb609d9c6ef0a1c1e732f1d6f32d3c14c2e1c554
Neil Opena <neil1114o@gmail.com>
2017-12-06 00:43:59 -0500

    Started DFS implementation using Stack

--- Homework_5/src/PrisonBreak.java
+++ Homework_5/src/PrisonBreak.java
@@ -37,10 +37,41 @@ public class PrisonBreak {
 class TestCase{
     private int N;
     private int[][] matrix;
+    private boolean[][] marked;
+    private Node start;
+    private Node end;
+
+    private class Node{
+        private int val;
+        private int row;
+        private int col;
+
+        public Node(int val, int row, int col){
+            this.val = val;
+            this.row = row;
+            this.col = col;
+        }
+    }
 
     public TestCase(int N, int[][] matrix){
         this.N = N;
         this.matrix = matrix;
+        this.marked = new boolean[N][N];
+
+        start = new Node(matrix[0][0], 0, 0);
+        end = new Node(matrix[N - 1][N - 1], N - 1, N - 1);
+
+        //initialize marked matrix where 1 = false
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(matrix[i][j] == 1){
+                    marked[i][j] = true;
+                }else{
+                    marked[i][j] = false;
+                }
+            }
+        }
+
     }
 
     public void printMatrix(){
@@ -50,6 +81,43 @@ class TestCase{
             }
             System.out.println();
         }
+    }
+
+    public int getNumEscapePaths() {
+        int count = 0;
+        /*
+        If the first cell [0,0] and the last cell [N-1,N-1] contain motion detectors
+        John can't break out of the prison
+        */
+        if (start.val == 1 && end.val == 1) {
+            return count;
+        }
+
+        Stack<Node> myStack = new Stack<>(); //create stack for DFS
+        myStack.push(start); //push starting cell
+
+        Node vertex;
+        while(!myStack.isEmpty()){
+            vertex = myStack.pop();
+            int i = vertex.row;
+            int j = vertex.col;
+
+            if(!marked[i][j]){
+                marked[i][j] = true;
+                System.out.print(vertex.val + "-");
+
+                //check right neighbor
+                if(j + 1 < N){
+                    if(matrix[i][j + 1] == 0){
+                        int val = matrix[i][j + 1];
+                        myStack.push(new Node(val, i, j + 1));
+                    }
+                }
+
+            }
+        }
+
+        return count;
     }
 
 }
