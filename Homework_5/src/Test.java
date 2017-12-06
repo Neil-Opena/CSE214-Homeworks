5877f08170e2fb598666fd52bc0f5029d1da04ac
Neil Opena <neil1114o@gmail.com>
2017-12-05 23:31:11 -0500

    Initialized homework 5 folder

---
+++ Homework_5/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args){
+        int[] arr = {1,2,3};
+        for(int num : arr){
+            System.out.println("Num " + num);
+        }
+    }
+}
+
